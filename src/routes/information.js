ca7819a4303d7a7c4fb7169e90533b3c9c36bfa7
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-06-21 23:47:08 -0300

    integrated POST, PATCH and DELETE about

--- src/routes/information.js
+++ src/routes/information.js
@@ -19,11 +19,11 @@ router.get('/about', async (req, res) => {
   }
 });
 
-router.delete('/about/:id', async (req, res) => {
-  const { id } = req.params;
+router.delete('/about/:_id', async (req, res) => {
+  const { _id } = req.params;
 
   try {
-    const result = await About.updateOne({ 'items._id': id }, { $pull: { items: { _id: id }} });
+    const result = await About.updateOne({ 'items._id': _id }, { $pull: { items: { _id }} });
 
     res.send(result);
   } catch(e) {
@@ -31,19 +31,30 @@ router.delete('/about/:id', async (req, res) => {
   }
 });
 
-router.patch('/about/:id', async (req, res) => {
-  // const result = await About.findOneAndUpdate({ 'content._id': mongoose.ObjectId('5d0586ac884c0d2ae427a565')}, { 'content.$.text': 'nada' });
-  console.log(req.params.id);
-  console.log(req.body);
+router.patch('/about/:_id', async (req, res) => {
+  const { _id } = req.params;
 
-  res.send();
+  try {
+    const result = await About.updateOne({ 'items._id': _id }, { 'items.$': { ...req.body, _id }});
+
+    res.send(result);
+  } catch(e) {
+    res.status(500).send({ error: 'Error editing information' });
+  }
 });
 
 router.post('/about', async (req, res) => {
-  // const result = await About.findOneAndUpdate({ 'content._id': mongoose.ObjectId('5d0586ac884c0d2ae427a565')}, { 'content.$.text': 'nada' });
-  console.log(req.body);
+  const { payload, groupId } = req.body;
 
-  res.send({ ...req.body, _id: new mongoose.Types.ObjectId() });
+  const _id = new mongoose.Types.ObjectId();
+
+  try  {
+    await About.updateOne({ _id: groupId }, { $push: { items: { ...payload, _id } } });
+
+    res.send({ ...payload, _id });
+  } catch(e) {
+    res.status(500).send({ error: 'Error creating information' });
+  }
 });
 
 router.get('/languages', async (req, res) => {
