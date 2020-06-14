b9eca05dc9a1b51c3be59fff3e5f4e62aff8e5f0
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2020-06-14 20:01:53 -0300

    trimmed chalk methods

--- src/utils/chalk.js
+++ src/utils/chalk.js
@@ -1,19 +1,19 @@
 const chalk = require('chalk');
 
 const blue = (text) => {
-  console.log(chalk.blue.bold(` ${text} `));
+  console.log(chalk.blue.bold(text));
 };
 
 const red = (text) => {
-  console.log(chalk.red.bold(` ${text} `));
+  console.log(chalk.red.bold(text));
 };
 
 const green = (text) => {
-  console.log(chalk.green.bold(` ${text} `));
+  console.log(chalk.green.bold(text));
 };
 
 const yellow = (text) => {
-  console.log(chalk.yellow.bold(` ${text} `));
+  console.log(chalk.yellow.bold(text));
 };
 
 module.exports = {
